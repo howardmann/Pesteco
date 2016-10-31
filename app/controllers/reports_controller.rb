34577607c44014ecc0e584984c999df613122726
howardmann <howardmann27@gmail.com>
2016-10-31 14:23:18 +1100

    Added cloudinary gem file

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,11 +15,23 @@ class ReportsController < ApplicationController
   def create
     @building = Building.find(params[:building_id])
     # TODO: replace with cloudinary upload of pdf files
-    @report = Report.create(report_params)
+    @report = Report.new(report_params)
+    # First, check to see if the user has an attached an image/ pdf for uploading
+    if params[:file]
+      # Then call Cloudinary's upload method passing in the file as a paramater
+      req = Cloudinary::Uploader.upload(params[:file], :public_id => "pesteco/#{@report.id}")
+      # Using the public_id of the Cloudinary file allows us to use Cloudinary's powerful transformation methods
+      @report.pdf = req['public_id']
+    end
+    # Save image and append to building
+    @report.save
     @building.reports << @report
+
     if @report.save
+      flash[:success] = "New report uploaded"
       redirect_to building_path(@building)
     else
+      flash[:error] = "Upload unsuccessful"
       render :new
     end
   end
@@ -30,10 +42,21 @@ class ReportsController < ApplicationController
 
   def update
     @report = Report.find(params[:id])
-    @report.update(report_params)
+
+    # Similar to create, check if file has been uploaded and if so upload to pdf
+    if params[:file]
+      req = Cloudinary.Uploader.upload(params[:file], :public_id => "pesteco/#{@report.id}")
+      @report.pdf = req['public_id']
+    end
+    # We use update_attributes to not save straight away, similar to new vs. create
+    @report.update_attributes(report_params)
+    @report.save
+
     if @report.save
+      flash[:success] = "Report updated"
       redirect_to building_path(@report.building)
     else
+      flash[:error] = "Update unsuccesful"
       render :edit
     end
   end
