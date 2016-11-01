029512af25586dca716ce4913779feb4e54fcda0
howardmann <howardmann27@gmail.com>
2016-11-01 15:05:32 +1100

    changed production sendgrid url

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,7 @@
 ActionMailer::Base.raise_delivery_errors = false
 
+ActionMailer::Base.default_url_options = { :host => 'pesteco.herokuapp.com' }
+
 ActionMailer::Base.smtp_settings = {
   :user_name => Rails.application.secrets.sendgrid_username,
   :password => Rails.application.secrets.sendgrid_password,
