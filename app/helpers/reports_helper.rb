98af1c2742f4fb72f9c9052a0e8df5283f6fc6eb
howardmann <howardmann27@gmail.com>
2016-11-02 14:39:55 +1100

    Added report mailto and download feature

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,9 @@
 module ReportsHelper
+
+  def format_email_report(report)
+    report_url = cl_image_path(report.pdf)
+
+    mail_to('', 'Email', {:subject => "Pesteco report: #{report.building.name}", :cc => 'howardmann27@gmail.com', :body => "Please copy the secure link below into your internet browser to view your Pesteco report: \n \n #{report_url}", :encode => 'javascript'})
+  end
+
 end
