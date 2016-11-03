12a19df19c3f721171d56d8aa4b6d0aaa5ee5553
howardmann <howardmann27@gmail.com>
2016-11-03 12:04:30 +1100

    Added grid system and formatted flash helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def format_flash_message
+    if flash[:success]
+      flash[:success].html_safe
+    elsif flash[:error]
+      flash[:error].html_safe
+    end
+  end
+
 end
